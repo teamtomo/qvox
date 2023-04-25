db393fb17272a97ccbc4bc868878e50aade816b4
Benjamin Barad <benjamin.barad@gmail.com>
2023-04-25 14:22:42 -0700

    Fixes to build and test

--- tests/test_qvox.py
+++ tests/test_qvox.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 test_array = np.zeros((2,3,4))
 test_array[0,0,:] = 1
 test_array[1,:,3] = 2
@@ -8,7 +10,6 @@ def test_resample():
     """
     Tests the resample function from the qvox.sampling module.
     """
-    import numpy as np
     from qvox.sampling import resample
     expected_output = np.zeroes((4,6,8))
     expected_output[0:2,0:2,:] = 1
